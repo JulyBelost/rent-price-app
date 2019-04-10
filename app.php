884aa4a5d640f8fec8438f71565a89dc3f8c7e9b
JulyBelost <julybelost@gmail.com>
2019-04-10 18:46:01 +0300

    added district factor to formula

--- app.php
+++ app.php
@@ -37,7 +37,7 @@ $app->post('/calc', function (Request $request) use ($app) {
     $db = $app['database'];
     $twig = $app['twig'];
 
-    $coords       = $request->request->get('coords');
+    $coords_str       = $request->request->get('coords');
     $address      = $request->request->get('address');
     $floor        = $request->request->get('floor');
     $floor_total  = $request->request->get('floor_total');
@@ -58,7 +58,7 @@ $app->post('/calc', function (Request $request) use ($app) {
             ':time' => $time,
             ':ip' => $ip,
             ':user_agent' => $user_agent,
-            ':coords' => $coords,
+            ':coords' => $coords_str,
             ':address' => $address,
             ':floor' => $floor,
             ':floor_total' => $floor_total,
@@ -68,10 +68,28 @@ $app->post('/calc', function (Request $request) use ($app) {
         ]);
         syslog(LOG_INFO , $logm);
     }
-
     // коэффициенты для рассчета величины арендной платы
     $base_cost_unit = 350;
-    $district_factor = 1;
+    
+    // Moscow center coords 55°45'07.3"N 37°37'27.0"E
+    $lat_c = 55.752028;
+    $long_c = 37.624167;
+    $coords = explode(", ", $coords_str);
+    $distance_to_center = getDistance($lat_c, $long_c, $coords[0], $coords[1]);
+    $to_bulv = 1.5;
+    $to_sad = 2.5;
+    $to_ttk = 5;
+    $to_mcad = 17;
+
+    if($distance_to_center <= $to_bulv){
+        $district_factor = 1.5;
+    }else if($distance_to_center <= $to_ttk){
+        $district_factor = $distance_to_center <=  $to_sad? 1.2 : 1;
+    } else {
+        $district_factor = $distance_to_center <= $to_mcad ? 0.9 : 0.7;
+    }
+
+     = 1;
     $floor_factor = $floor == $floor_total || $floor == 1 ? 0.8 : 1;
     $ren_factor_arr = [
         'без ремонта' => 0.6,
@@ -107,6 +125,18 @@ $app['database'] = function () use ($app) {
 
     return $db;
 };
+
+function getDistance($lat1, $long1, $lat2, $long2) {
+    if($lat1 == $lat2 && $long1 == $long2){
+        return 0;
+    }
+    $to_rad = M_PI / 180;
+    $dist = acos(sin($lat1 * $rad)
+        * sin($lat2 * $rad) +  cos($lat1 * $rad)
+        * cos($lat2 * $rad) * cos(($long1 - $long2) * $rad));
+    $dist_km = $dist / $rad * 60 *  1.853;
+    return $dist_km;
+}
 
 return $app;
 ?>
